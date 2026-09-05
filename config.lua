5e1ff3907f52dec2db37628b0a74d05b4b531c3a
jKalfis <jorgelima.kalfis@gmail.com>
2026-09-05 12:50:16 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,107 +1,27 @@
+------------------------------------------------------------
 -- pfUI-DQB
 -- config.lua
+--
 -- Default configuration for DQB.
 -- GUI registration is handled by gui.lua.
+------------------------------------------------------------
+
 local DQB = pfUI and pfUI.dqb
 
 if not DQB then
     return
 end
 
--- Helpers
-local function GetPFUIFont()
-    if pfUI and pfUI.font_default then
-        return pfUI.font_default
-    end
 
-    if pfUI_config
-    and pfUI_config.global
-    and pfUI_config.global.font_default then
-        return pfUI_config.global.font_default
-    end
+------------------------------------------------------------
+-- Default configuration
+------------------------------------------------------------
 
-    return "Interface\\AddOns\\pfUI\\fonts\\Myriad-Pro.ttf"
-end
-
-local function GetPFUIBackgroundColor()
-    if pfUI_config
-    and pfUI_config.global
-    and pfUI_config.global.background_color then
-        return pfUI_config.global.background_color
-    end
-
-    return "0,0,0,1"
-end
-
-local function GetPFUIBackgroundAlpha()
-    if pfUI_config
-    and pfUI_config.global
-    and pfUI_config.global.background_alpha ~= nil then
-        return tostring(pfUI_config.global.background_alpha)
-    end
-
-    return "0.75"
-end
-
--- Module defaults
-local function InitializeModule(module)
-
-    if not module then
-        return
-    end
-
-    -- No style selected by default.
-    if module.use_pfui_style == nil then
-        module.use_pfui_style = "0"
-    end
-
-    if module.use_custom_style == nil then
-        module.use_custom_style = "0"
-    end
-
-    -- pfUI Style
-    if module.pfui_background_alpha == nil then
-        module.pfui_background_alpha = GetPFUIBackgroundAlpha()
-    end
-
-    -- Custom Style
-    if not module.custom then
-        module.custom = {}
-    end
-
-    local custom = module.custom
-
-    if custom.remove_parchment == nil then
-        custom.remove_parchment = "1"
-    end
-
-    if custom.background_color == nil then
-        custom.background_color = GetPFUIBackgroundColor()
-    end
-
-    if custom.background_alpha == nil then
-        custom.background_alpha = GetPFUIBackgroundAlpha()
-    end
-
-    if custom.font == nil then
-        custom.font = GetPFUIFont()
-    end
-
-    -- DQB visual defaults
-    -- Titles = Gold
-    -- Text   = White
-    if custom.title_color == nil then
-        custom.title_color = "1,0.82,0,1"
-    end
-
-    if custom.text_color == nil then
-        custom.text_color = "1,1,1,1"
-    end
-
-end
-
--- Initialize configuration
 function DQB:InitializeConfig()
+
+    --------------------------------------------------------
+    -- Root
+    --------------------------------------------------------
 
     if not pfUI_config then
         pfUI_config = {}
@@ -111,7 +31,11 @@ function DQB:InitializeConfig()
         pfUI_config.dqb = {}
     end
 
-    -- General
+
+    --------------------------------------------------------
+    -- GENERAL
+    --------------------------------------------------------
+
     if not pfUI_config.dqb.general then
         pfUI_config.dqb.general = {}
     end
@@ -122,36 +46,529 @@ function DQB:InitializeConfig()
         general.enable = "1"
     end
 
-    -- Quest & Gossip
+
+    --------------------------------------------------------
+    -- QUEST & GOSSIP
+    --------------------------------------------------------
+
     if not pfUI_config.dqb.questgossip then
         pfUI_config.dqb.questgossip = {}
     end
 
-    InitializeModule(
-        pfUI_config.dqb.questgossip
-    )
+    local questgossip = pfUI_config.dqb.questgossip
 
-    -- Quest Log
+
+    --------------------------------------------------------
+    -- Use pfUI Style
+    --------------------------------------------------------
+
+    if questgossip.use_pfui_style == nil then
+        questgossip.use_pfui_style = "0"
+    end
+
+
+    --------------------------------------------------------
+    -- Use Custom Style
+    --------------------------------------------------------
+
+    if questgossip.use_custom_style == nil then
+        questgossip.use_custom_style = "0"
+    end
+
+
+    --------------------------------------------------------
+    -- pfUI Style
+    --------------------------------------------------------
+
+    if questgossip.pfui_background_alpha == nil then
+
+        if pfUI_config.global
+        and pfUI_config.global.background_alpha ~= nil then
+
+            questgossip.pfui_background_alpha =
+                tostring(
+                    pfUI_config.global.background_alpha
+                )
+
+        else
+
+            questgossip.pfui_background_alpha =
+                "0.75"
+
+        end
+
+    end
+
+
+    --------------------------------------------------------
+    -- Custom Style
+    --------------------------------------------------------
+
+    if not questgossip.custom then
+        questgossip.custom = {}
+    end
+
+    local custom = questgossip.custom
+
+
+    --------------------------------------------------------
+    -- Remove Blizzard Parchment Texture
+    --------------------------------------------------------
+
+    if custom.remove_parchment == nil then
+        custom.remove_parchment = "1"
+    end
+
+
+    --------------------------------------------------------
+    -- Background Color
+    --------------------------------------------------------
+
+    if custom.background_color == nil then
+
+        if pfUI_config.global
+        and pfUI_config.global.background then
+
+            custom.background_color =
+                pfUI_config.global.background
+
+        elseif pfUI_config.global
+        and pfUI_config.global.background_color then
+
+            custom.background_color =
+                pfUI_config.global.background_color
+
+        else
+
+            custom.background_color =
+                "0,0,0,1"
+
+        end
+
+    end
+
+
+    --------------------------------------------------------
+    -- Background Opacity
+    --------------------------------------------------------
+
+    if custom.background_alpha == nil then
+
+        if pfUI_config.global
+        and pfUI_config.global.background_alpha ~= nil then
+
+            custom.background_alpha =
+                tostring(
+                    pfUI_config.global.background_alpha
+                )
+
+        else
+
+            custom.background_alpha =
+                "0.75"
+
+        end
+
+    end
+
+
+    --------------------------------------------------------
+    -- Font
+    --------------------------------------------------------
+
+    if custom.font == nil then
+
+        if pfUI.font_default then
+
+            custom.font =
+                pfUI.font_default
+
+        elseif pfUI_config.global
+        and pfUI_config.global.font_default then
+
+            custom.font =
+                pfUI_config.global.font_default
+
+        else
+
+            custom.font =
+                "Interface\\AddOns\\pfUI\\fonts\\Myriad-Pro.ttf"
+
+        end
+
+    end
+
+
+    --------------------------------------------------------
+    -- Title Color
+    --------------------------------------------------------
+
+    if custom.title_color == nil then
+        custom.title_color = "1,0.84,0"
+    end
+
+
+    --------------------------------------------------------
+    -- Text Color
+    --------------------------------------------------------
+
+    if custom.text_color == nil then
+        custom.text_color = "1,1,1"
+    end
+
+
+    --------------------------------------------------------
+    -- QUEST LOG
+    --------------------------------------------------------
+
     if not pfUI_config.dqb.questlog then
         pfUI_config.dqb.questlog = {}
     end
 
-    InitializeModule(
-        pfUI_config.dqb.questlog
-    )
+    local questlog = pfUI_config.dqb.questlog
 
-    -- Books
+
+    --------------------------------------------------------
+    -- Use pfUI Style
+    --------------------------------------------------------
+
+    if questlog.use_pfui_style == nil then
+        questlog.use_pfui_style = "0"
+    end
+
+
+    --------------------------------------------------------
+    -- Use Custom Style
+    --------------------------------------------------------
+
+    if questlog.use_custom_style == nil then
+        questlog.use_custom_style = "0"
+    end
+
+
+    --------------------------------------------------------
+    -- pfUI Style
+    --------------------------------------------------------
+
+    if questlog.pfui_background_alpha == nil then
+
+        if pfUI_config.global
+        and pfUI_config.global.background_alpha ~= nil then
+
+            questlog.pfui_background_alpha =
+                tostring(
+                    pfUI_config.global.background_alpha
+                )
+
+        else
+
+            questlog.pfui_background_alpha =
+                "0.75"
+
+        end
+
+    end
+
+
+    --------------------------------------------------------
+    -- Custom Style
+    --------------------------------------------------------
+
+    if not questlog.custom then
+        questlog.custom = {}
+    end
+
+    local custom = questlog.custom
+
+
+    --------------------------------------------------------
+    -- Remove Blizzard Parchment Texture
+    --------------------------------------------------------
+
+    if custom.remove_parchment == nil then
+        custom.remove_parchment = "1"
+    end
+
+
+    --------------------------------------------------------
+    -- Background Color
+    --------------------------------------------------------
+
+    if custom.background_color == nil then
+
+        if pfUI_config.global
+        and pfUI_config.global.background then
+
+            custom.background_color =
+                pfUI_config.global.background
+
+        elseif pfUI_config.global
+        and pfUI_config.global.background_color then
+
+            custom.background_color =
+                pfUI_config.global.background_color
+
+        else
+
+            custom.background_color =
+                "0,0,0,1"
+
+        end
+
+    end
+
+
+    --------------------------------------------------------
+    -- Background Opacity
+    --------------------------------------------------------
+
+    if custom.background_alpha == nil then
+
+        if pfUI_config.global
+        and pfUI_config.global.background_alpha ~= nil then
+
+            custom.background_alpha =
+                tostring(
+                    pfUI_config.global.background_alpha
+                )
+
+        else
+
+            custom.background_alpha =
+                "0.75"
+
+        end
+
+    end
+
+
+    --------------------------------------------------------
+    -- Font
+    --------------------------------------------------------
+
+    if custom.font == nil then
+
+        if pfUI.font_default then
+
+            custom.font =
+                pfUI.font_default
+
+        elseif pfUI_config.global
+        and pfUI_config.global.font_default then
+
+            custom.font =
+                pfUI_config.global.font_default
+
+        else
+
+            custom.font =
+                "Interface\\AddOns\\pfUI\\fonts\\Myriad-Pro.ttf"
+
+        end
+
+    end
+
+
+    --------------------------------------------------------
+    -- Title Color
+    --------------------------------------------------------
+
+    if custom.title_color == nil then
+        custom.title_color = "1,0.84,0"
+    end
+
+
+    --------------------------------------------------------
+    -- Text Color
+    --------------------------------------------------------
+
+    if custom.text_color == nil then
+        custom.text_color = "1,1,1"
+    end
+
+
+    --------------------------------------------------------
+    -- BOOKS
+    --------------------------------------------------------
+    --
+    -- Books corresponds to the original pfUI itemtext.lua.
+    --------------------------------------------------------
+
     if not pfUI_config.dqb.books then
         pfUI_config.dqb.books = {}
     end
 
-    InitializeModule(
-        pfUI_config.dqb.books
-    )
+    local books = pfUI_config.dqb.books
 
-    -- Merchant
-    -- Reserved for later, it may be not needed.
+
+    --------------------------------------------------------
+    -- Use pfUI Style
+    --------------------------------------------------------
+
+    if books.use_pfui_style == nil then
+        books.use_pfui_style = "0"
+    end
+
+
+    --------------------------------------------------------
+    -- Use Custom Style
+    --------------------------------------------------------
+
+    if books.use_custom_style == nil then
+        books.use_custom_style = "0"
+    end
+
+
+    --------------------------------------------------------
+    -- pfUI Style
+    --------------------------------------------------------
+
+    if books.pfui_background_alpha == nil then
+
+        if pfUI_config.global
+        and pfUI_config.global.background_alpha ~= nil then
+
+            books.pfui_background_alpha =
+                tostring(
+                    pfUI_config.global.background_alpha
+                )
+
+        else
+
+            books.pfui_background_alpha =
+                "0.75"
+
+        end
+
+    end
+
+
+    --------------------------------------------------------
+    -- Custom Style
+    --------------------------------------------------------
+
+    if not books.custom then
+        books.custom = {}
+    end
+
+    local custom = books.custom
+
+
+    --------------------------------------------------------
+    -- Remove Blizzard Parchment Texture
+    --------------------------------------------------------
+
+    if custom.remove_parchment == nil then
+        custom.remove_parchment = "1"
+    end
+
+
+    --------------------------------------------------------
+    -- Background Color
+    --------------------------------------------------------
+
+    if custom.background_color == nil then
+
+        if pfUI_config.global
+        and pfUI_config.global.background then
+
+            custom.background_color =
+                pfUI_config.global.background
+
+        elseif pfUI_config.global
+        and pfUI_config.global.background_color then
+
+            custom.background_color =
+                pfUI_config.global.background_color
+
+        else
+
+            custom.background_color =
+                "0,0,0,1"
+
+        end
+
+    end
+
+
+    --------------------------------------------------------
+    -- Background Opacity
+    --------------------------------------------------------
+
+    if custom.background_alpha == nil then
+
+        if pfUI_config.global
+        and pfUI_config.global.background_alpha ~= nil then
+
+            custom.background_alpha =
+                tostring(
+                    pfUI_config.global.background_alpha
+                )
+
+        else
+
+            custom.background_alpha =
+                "0.75"
+
+        end
+
+    end
+
+
+    --------------------------------------------------------
+    -- Font
+    --------------------------------------------------------
+
+    if custom.font == nil then
+
+        if pfUI.font_default then
+
+            custom.font =
+                pfUI.font_default
+
+        elseif pfUI_config.global
+        and pfUI_config.global.font_default then
+
+            custom.font =
+                pfUI_config.global.font_default
+
+        else
+
+            custom.font =
+                "Interface\\AddOns\\pfUI\\fonts\\Myriad-Pro.ttf"
+
+        end
+
+    end
+
+
+    --------------------------------------------------------
+    -- Title Color
+    --------------------------------------------------------
+
+    if custom.title_color == nil then
+        custom.title_color = "1,0.84,0"
+    end
+
+
+    --------------------------------------------------------
+    -- Text Color
+    --------------------------------------------------------
+
+    if custom.text_color == nil then
+        custom.text_color = "1,1,1"
+    end
+
+
+    --------------------------------------------------------
+    -- MERCHANT
+    --------------------------------------------------------
+    --
+    -- Kept untouched for now.
+    --------------------------------------------------------
+
     if not pfUI_config.dqb.merchant then
         pfUI_config.dqb.merchant = {}
     end
+
 end
